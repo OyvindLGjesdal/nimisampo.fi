f3eb0a09e412ccc0be49c51ee341366152706cdb
Oyvind Gjesdal <oyvind.gjesdal@ub.uib.no>
2019-10-24 14:03:35 +0200

    change default zoom

--- src/client/components/facet_results/GMap.js
+++ src/client/components/facet_results/GMap.js
@@ -4,7 +4,7 @@ import HeatmapLayer from 'react-google-maps/lib/components/visualization/Heatmap
 
 let GMap = withGoogleMap((props) =>
   <GoogleMap
-    defaultZoom={5}
+    defaultZoom={4}
     defaultCenter={{ lat: 65.21, lng: 12.4917 }}
   >
     <HeatmapLayer data={
